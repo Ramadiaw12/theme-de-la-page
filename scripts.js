7dcde2369a66e5ab1f210860869359101ff7828b
Super Rama <superrama86@gmail.com>
2025-01-02 15:51:59 +0000

    Thème de ma page

--- scripts.js
+++ scripts.js
@@ -1,34 +1,25 @@
 // TODO Selectionner le boutton avec l'ID 'theme Toggle'
+let button = document.getElementById("themeToggle");
 
-let themeToggle = document.getElementById("themeToggle");
 // TODO Créer une variable pour suivre l'état du thème(clair/sombre)
-
-let enfants = themeToggle.childNodes;
+let bg = document.querySelector("body");
 
 // TODO Ajouter un écouteur d'événement 'click' sur le boutton
-enfants.forEach(function (enfant){
-    enfant.className = "element";
-});
+let dark = "dark";
 
 //TODO Dans la fonction de l'écouteur:
 // TODO -chager la valeur --bg-color et --text-color en fonction du thème
 //TODO -mette à jour la variable d'état du thème
-let button = document.querySelector("button");
-// button.addEventListener("click", function(){
-//     enfants.forEach(function(enfant){
-//         enfant.className = enfant.className === "element" ? "dark" : "element";
-//     });
-
-// });
 let isDarkTheme = false;
-button.addEventListener("click", function(){
-    if (isDarkTheme){
-        document.documentElement.style.setProperty("--bg-color", "#000000");
-        document.documentElement.style.setProperty("--text-color", "#ffffff");
-    }else{
-        document.documentElement.style.setProperty("--bg-color","#ffffff");
-        document.documentElement.style.setProperty("--text-color", "#000000");
-    
+
+button.addEventListener("click", function() {
+    if(bg.classList.contains("dark")) {
+        bg.classList.remove("dark");
+    }
+    else{
+        // Passer au theme sombre
+        bg.classList.add("dark");
     };
+
 });
 
